70b52dabd2f40a329523cf1ce54c86494c9cdb33
Brandon K. Waltz <bwaltz6@gmail.com>
2023-09-25 13:02:39 -0400

    update components.js

--- src/components.js
+++ src/components.js
@@ -1,5 +1,9 @@
 import Search from './twig/components/search/Search.vue';
+import OverlayHero from './twig/components/overlay-hero/OverlayHero.vue';
+import ArticleGrid from './twig/components/article-grid/ArticleGrid.vue';
 
 export default {
     Search,
+    OverlayHero,
+    ArticleGrid,
 };
